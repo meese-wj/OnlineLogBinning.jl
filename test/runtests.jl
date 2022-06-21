45000035ae190e9ac5892de6247c68313c89195a
W. Joe Meese <meese022@umn.edu>
2022-06-21 16:04:54 -0500

    Add some constructor tests. Still WIP

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,45 @@ using OnlineLogBinning
 using Test
 using StaticArrays
 
+# Tested number types for OnlineLogBinning.jl
+OLB_tested_numbers = @SVector [ Int8, Int16, Int32, Int64, Int128,
+                                UInt8, UInt16, UInt32, UInt64, UInt128,
+                                Float16, Float32, Float64,
+                                BigInt, BigFloat,
+                                Rational,
+                                Bool,
+                                ComplexF16, ComplexF32, ComplexF64 ]
+
 @testset "OnlineLogBinning.jl" begin
     @testset "Constructors" begin
-        # PairAccumulator{Int}
-        @test let 
-            pacc = PairAccumulator{Int}()
-            pacc.fullpair == true && pacc.values == @MVector zeros(Int, 2)            
+
+        @testset "PairAccumulator" begin
+            for type ∈ OLB_tested_numbers
+                eval(quote
+                    @test let
+                        pacc = PairAccumulator{$type}()
+                        ( pacc.fullpair == true &&
+                          pacc.values == @MVector zeros($type, 2) )
+                    end
+                end)
+            end
         end
+        
+        @testset "LevelAccumulator" begin
+            for type ∈ OLB_tested_numbers
+                eval(quote
+                    @test let
+                        lacc = LevelAccumulator{$type}()
+                        ( lacc.level == zero(Int) &&
+                          lacc.num_bins == zero(Int) &&
+                          lacc.Taccum == zero($type) &&
+                          lacc.Saccum == zero($type) &&
+                          lacc.Paccum.fullpair == true &&
+                          lacc.Paccum.values == @MVector zeros($type, 2) )
+                    end
+                end)
+            end
+        end
+
     end
 end
